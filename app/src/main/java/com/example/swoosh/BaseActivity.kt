1336be6365d3fc79f5d54d55685bda57bf31a8f6
paul-romeo <countmedream@yahoo.com>
2020-09-22 21:27:37 -0700

    lesson 5

---
+++ app/src/main/java/com/example/swoosh/BaseActivity.kt
@@ -0,0 +1,39 @@
+package com.example.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+open class BaseActivity : AppCompatActivity() {
+    val TAG = "LifeCycle"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "${javaClass.simpleName} onCreate")
+
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "${javaClass.simpleName} onStart")
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "${javaClass.simpleName} onRestart")
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "${javaClass.simpleName} onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "${javaClass.simpleName} onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "${javaClass.simpleName} onDestroy")
+        super.onDestroy()
+    }
+}
